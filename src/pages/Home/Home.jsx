f41f852484c5c38ee73faf7644d9542f60e7cd76
knightoffcode <isaiaspereira103@yahoo.com>
2024-10-30 22:52:20 -0300

    #006 - Including components in home.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Highlight from '../../components/Highlight/Highlight'
+import React from 'react';
+import { useTheme } from '../../ThemeContext';
+import Header from '../../components/Header/';
+import Footer from '../../components/Footer/';
+import Main from '../../components/Main';
 
-/**
- *
- * @returns
- */
+
+
 
 function Home() {
+
+    const { darkTheme } = useTheme();
+
     return (
-        <div className='home-page'>
-            <Highlight />
-        </div>
-    )
+        <main className={`main ${darkTheme ? 'main--dark' : 'main--light'}`}>
+            <Header />
+            <Main />
+            <Footer />
+        </main>
+    );
 }
 
-export default Home
+export default Home;
